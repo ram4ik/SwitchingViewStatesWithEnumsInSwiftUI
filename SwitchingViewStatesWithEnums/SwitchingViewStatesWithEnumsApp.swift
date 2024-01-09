653ec8576b028976ca03ab4ec6531b1b335ff577
Ramill <ram4ik@hotmail.com>
2024-01-09 14:41:06 +0200

    Initial Commit

---
+++ SwitchingViewStatesWithEnums/SwitchingViewStatesWithEnumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchingViewStatesWithEnumsApp.swift
+//  SwitchingViewStatesWithEnums
+//
+//  Created by admin on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwitchingViewStatesWithEnumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
